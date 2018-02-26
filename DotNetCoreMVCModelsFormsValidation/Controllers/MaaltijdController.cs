7f534f1d70db6f5def57ea6840927f1b07672cfe
GillisjansRaf <raf.gillisjans@student.howest.be>
2018-02-26 10:23:03 +0100

    Add project files.

---
+++ DotNetCoreMVCModelsFormsValidation/Controllers/MaaltijdController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DotNetCoreMVCModelsFormsValidation.Models;
+using Newtonsoft.Json;
+
+namespace DotNetCoreMVCModelsFormsValidation.Controllers
+{
+    public class MaaltijdController : Controller
+    {
+        private List<Maaltijd> maaltijden = new List<Maaltijd>();
+        public MaaltijdController()
+        {
+            maaltijden.Add(
+                new Maaltijd
+                {
+                    Id =1,
+                    Type =MaaltijdType.Ontbijt,
+                    Naam ="Ontbijt1",
+                    Prijs =25
+                });
+            maaltijden.Add(
+                new Maaltijd
+                {
+                    Id = 10,
+                    Type = MaaltijdType.Lunch,
+                    Naam = "Dagmenu",
+                    Prijs = 35
+                });
+        }
+        public ViewResult Index()
+        {
+            if (TempData.Peek("maaltijden") != null)
+            {
+                maaltijden = JsonConvert.DeserializeObject<List<Maaltijd>>(TempData["maaltijden"].ToString());
+            }
+            return View(maaltijden);
+        }
+
+        [HttpGet]
+        public ViewResult Create()
+        {
+            return View(new Maaltijd { Type=MaaltijdType.Ontbijt });
+        }
+
+        [HttpPost]
+        public IActionResult Create(Maaltijd maaltijd)
+        {
+            if (ModelState.IsValid)
+            {
+                maaltijden.Add(maaltijd);
+                TempData["maaltijden"] = JsonConvert.SerializeObject(maaltijden);
+                TempData.Keep();
+                //return View("Finish", maaltijd);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(maaltijd);
+            }
+        }
+    }
+}
---
+++ DotNetCoreMVCModelsFormsValidation/Startup.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DotNetCoreMVCModelsFormsValidation
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().AddSessionStateTempDataProvider(); //http://www.binaryintellect.net/articles/090f0fe3-d52c-4386-9d30-6311c2d62738.aspx
+            services.AddMemoryCache();
+            services.AddSession();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseSession();
+            app.UseMvcWithDefaultRoute();
+            //app.UseMvc(routes =>
+            //{
+            //    routes.MapRoute(
+            //        name: "default",
+            //        template: "{controller=Home}/{action=Index}/{id?}");
+            //});
+        }
+    }
+}
